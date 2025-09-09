9720b5b48fad1add095b8b766353456f279df068
Peculiars <olaitandamilare230@gmail.com>
2025-09-09 10:25:49 +0100

    feat:initial folders and files structures

---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { ClerkProvider, SignInButton, SignUpButton, SignedIn, SignedOut, UserButton, } from "@clerk/nextjs";
+
+const ibmPlexSans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Pixiwise",
+  description: "AI-Powered Image Transformation and Management",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <ClerkProvider> 
+      <html lang="en">
+        <body className={`${ibmPlexSans.variable} antialiased`} >
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
+  );
+}

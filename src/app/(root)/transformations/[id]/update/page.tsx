329db6117e153078d6cc01952a278d0e0e80c42b
Peculiars <olaitandamilare230@gmail.com>
2025-09-10 19:54:09 +0100

    feat: implement recolor and object removal

--- src/app/(root)/transformations/[id]/update/page.tsx
+++ src/app/(root)/transformations/[id]/update/page.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { redirect } from "next/navigation";
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { getUserById } from "@/lib/actions/user.actions";
 
-const UpdateTransformationPage = () => {
+import { SearchParamProps, TransformationTypeKey } from "../../../../../../types";
+import { currentUser } from "@clerk/nextjs/server";
+import { getImageById } from "@/lib/actions/image.action";
+import { transformationTypes } from "../../../../../../constants";
+
+const Page = async ({ params }: SearchParamProps) => {
+  const { id } = await params;
+  const userInfo = await currentUser();
+    const userId = userInfo?.id; 
+
+  if (!userId) redirect("/sign-in");
+
+  const user = await getUserById(userId);
+  const image = await getImageById(id);
+
+  const transformation =
+    transformationTypes[image.transformationType as TransformationTypeKey];
+
   return (
-    <div>UpdateTransformationPage</div>
-  )
-}
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
 
-export default UpdateTransformationPage
+      <section className="mt-10">
+        <TransformationForm
+          action="Update"
+          userId={user._id}
+          type={image.transformationType as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+          config={image.config}
+          data={image}
+        />
+      </section>
+    </>
+  );
+};
+
+export default Page;
